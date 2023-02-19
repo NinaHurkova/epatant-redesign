2b8c839eff879fe41c422c7d7590b21fc4113878
Nina Hurkova <gurkovanu@gmail.com>
2023-02-19 13:43:56 +0200

    add main gallery

--- htdocs/js/main.js
+++ htdocs/js/main.js
@@ -40,3 +40,11 @@ $(document).ready(function() {
     })
 });
 
+// https://photoswipe.com/
+var lightbox = new PhotoSwipeLightbox({
+    gallery: '.gallery',
+    children: 'a',
+    // dynamic import is not supported in UMD version
+    pswpModule: PhotoSwipe
+});
+lightbox.init();
