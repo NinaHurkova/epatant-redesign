e9f86c49e838fd6c3ecff3f47de6feba35658cd0
Nina Hurkova <gurkovanu@gmail.com>
2023-02-24 20:38:16 +0200

    theme and accent switcher

--- htdocs/js/main.js
+++ htdocs/js/main.js
@@ -48,3 +48,56 @@ var lightbox = new PhotoSwipeLightbox({
     pswpModule: PhotoSwipe
 });
 lightbox.init();
+
+//Change color scheme
+var darkMode = false;
+
+// default to system setting
+if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+	darkMode = true;
+}
+
+// preference from localStorage should overwrite
+if (localStorage.getItem('theme') === 'dark') {
+	darkMode = true;
+} else if (localStorage.getItem('theme') === 'light') {
+	darkMode = false;
+}
+
+if (darkMode) {
+	document.documentElement.setAttribute("theme", "dark");
+    document.getElementById('theme-toggle').classList.add('is-dark');
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    document.getElementById('theme-toggle').addEventListener('click', () => {
+		if(document.documentElement.hasAttribute("theme")){
+            document.documentElement.removeAttribute("theme");
+            localStorage.removeItem('theme');
+            document.getElementById('theme-toggle').classList.remove('is-dark');
+        }
+          else{
+            document.documentElement.setAttribute("theme", "dark");
+            localStorage.setItem('theme', 1);
+            document.getElementById('theme-toggle').classList.add('is-dark');
+        }
+    	localStorage.setItem('theme', document.documentElement.hasAttribute("theme") ? 'dark' : 'light');
+	});
+
+});
+
+//Change accent
+document.documentElement.setAttribute('accent',localStorage.getItem('accentColor'));
+
+const colors= document.querySelectorAll('.colors');
+
+colors.forEach(function(color) {
+    color.addEventListener('click',function() {
+        let accentColor = color.dataset.color;
+        document.documentElement.setAttribute('accent',accentColor);
+
+        localStorage.setItem('accentColor',color.dataset.color);
+
+    })
+})
