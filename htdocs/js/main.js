a502cb57f4b937ce9757cd57f63e159740555b5e
Nina Hurkova <gurkovanu@gmail.com>
2023-02-25 18:03:20 +0200

    services page

--- htdocs/js/main.js
+++ htdocs/js/main.js
@@ -1,25 +1,11 @@
 // Language switcher
 function chooseLanguage(){
-    if (window.matchMedia("(max-width: 1023px)").matches){
-        $(".lang-menu-list li").on("click", function(){
-            $(".lang-menu-list li").removeClass('selected');
-            $(this).addClass('selected');
-        });
-	}else{
-        $(".lang-menu").hover(function(){
-            $(".lang-menu-list").addClass("active");  //Add the active class to the area is hovered
-        }, function () {
-            $(".lang-menu-list").removeClass("active");
-        });
-        $(".lang-menu-list li").on("click", function(){
-            //select lang and apply changes
-            $lang = $(this).text();
-            $(".lang-menu-selected").text($lang);
-        });
-    }
+    $(".lang-menu-list li").on("click", function(){
+        $(".lang-menu-list li").removeClass('selected');
+        $(this).addClass('selected');
+    });
 }
-$(document).ready(chooseLanguage);
-$(window).on('resize',chooseLanguage);
+chooseLanguage();
 
 //Mobile menu toggle
 $("#menuToggle").on("click",function(){
